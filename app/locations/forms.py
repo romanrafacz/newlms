68e0c57710570ca54a0b1c3b8183ffad2239a1f5
shoemonkey <romanr32@gmail.com>
2014-10-08 21:35:41 +0200

    state select field

--- app/locations/forms.py
+++ app/locations/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import Form
-from wtforms.fields import TextField, IntegerField
+from wtforms.fields import TextField, IntegerField, SelectField
 from wtforms import validators
 
 strip_filter = lambda x: x.strip() if x else None
@@ -7,13 +7,14 @@ strip_filter = lambda x: x.strip() if x else None
 
 class LocationForm(Form):
     location = TextField('location', filters=[strip_filter])
-    facilityname = TextField()
-    address = TextField()
-    suite = TextField()
-    city = TextField()
-    state = TextField()
-    zipcode = TextField()
-    phone = TextField()
+    facilityname = TextField('facilityname', filters=[strip_filter])
+    address = TextField('address', filters=[strip_filter])
+    suite = TextField('suite', filters=[strip_filter])
+    city = TextField('city', filters=[strip_filter])
+    ###state = SelectField('state', filters=[('1', 'AL'), ('2', 'IL'), ('3', 'CA'), ('4', 'MI')])
+    state = SelectField(u'state', coerce=int)
+    zipcode = TextField('zipcode', filters=[strip_filter])
+    phone = TextField('phone', filters=[strip_filter])
 
     def validate_on_submit(*arg, **kwargs):
         return True
--- app/locations/views.py
+++ app/locations/views.py
@@ -1,4 +1,5 @@
 from flask import render_template, Blueprint, request, url_for, flash, redirect
+from wtforms.ext.sqlalchemy.fields import QuerySelectField
 
 from app.models import Location
 from app.locations.forms import LocationForm
@@ -11,23 +12,33 @@ locations_blueprint = Blueprint(
 
 @locations_blueprint.route('/locations', methods=['POST', 'GET'])
 def locations():
+    """
+    Location/Home - List all locations
+    """
     Form = LocationForm()
+    Form.state.choices = [(g.id, g.state) for g in Location.query.order_by('state')]
     ibmlocations = db.session.query(Location).all()
     return render_template('locations.html', locations=ibmlocations, form=Form)
 
 @locations_blueprint.route('/searchbylocation/<int:locationid>', methods=['POST', 'GET'])
 def searchbylocation(locationid):
+    """
+    Search by Location
+    """
     searchbylocation = db.session.query(Location).filter_by(id=locationid).first()
     return render_template('searchbylocation.html', location=searchbylocation)
 
 @locations_blueprint.route('/addlocation', methods=['POST', 'GET'])
 def addlocation():
+    """
+     Add Location
+    """
     message = ""
     if request.method == "POST":
         db.session.add(Location(location=request.form['location'], facilityname=request.form['facilityname'], address=request.form['address'], suite=request.form['suite'], city=request.form['city'], state=request.form['state'], zipcode=request.form['zipcode'], phone=request.form['phone']))
         db.session.commit()
         flash('successful add')
-        return redirect('addlocation.html')
+        return redirect('locations.html')
     else:
         flash("please try again")
         return redirect('locations.html')
@@ -35,20 +46,33 @@ def addlocation():
     message= "no dice, try again"
     return render_template('addlocation.html', message=message)
 
-@locations_blueprint.route('/editlocation', methods=['POST', 'GET'])
-def editlocation():
-    Form = LocationForm()
-    locationdata = Location.query.get(1)
-    Form.populate_obj(request.POST, locaitondata)
+@locations_blueprint.route('/editlocation/<int:locationid>', methods=['POST', 'GET'])
+def editlocation(locationid):
+    """
+    Edit Location
+    """
+    class Form(LocationForm):
+        pass
+
+    Form.location = request.form['location']
+    #location = Location.query.get(1)
+    form = Form()
+    form.state.choices = [(g.int, g.state) for g in Location.query.order_by('state')]
     return render_template('editlocations.html', form=Form)
 
 def delete_location(locationid):
+    """
+    Function to delete location
+    """
     db.session.query(Location).filter(Location.id==locationid).delete()
     db.session.commit()
 
 
 @locations_blueprint.route('/deletelocation/<int:locationid>', methods=['POST', 'GET'])
 def deletelocation(locationid):
+    """
+    Delete location
+    """
     if delete_location(locationid):
         flash('success')
         return redirect('locations')
