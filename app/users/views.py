dbc2d57b4b59d7decf21cdc7c339bd6881676064
shoemonkey <romanr32@gmail.com>
2014-10-10 02:50:05 +0200

    form page set

--- app/users/views.py
+++ app/users/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect
+from flask import Blueprint, render_template, redirect, url_for, request
 from app.users.forms import UserLoginForm
 
 users_blueprint = Blueprint(
@@ -8,4 +8,9 @@ users_blueprint = Blueprint(
 @users_blueprint.route('/users_login', methods=['POST', 'GET'])
 def users_login():
     LoginForm = UserLoginForm()
-    return render_template('users_index.html', form=LoginForm)
+
+    if request.method == 'GET':
+        return render_template('users_index.html', form=LoginForm)
+    username = request.form['username']
+    password = request.form['password']
+    return redirect(request.args.get('next') or url_for('app.locations.locations'))
